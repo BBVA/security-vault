158bc23ba038cde7f5d95174a373fa74ed994063
ROBERTO  MUÑOZ <roberto.munoz.fernandez.contractor@bbva.com>
2016-11-14 11:02:01 +0100

    Quitamos todo el legacy  de example secret api porque ya no es necesario.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM ubuntu
 
 ADD ./cloudframe-security-vault /usr/bin/
-ADD ./certs/ca.cert /tmp/ca.cert
-ADD ./certs/public.key /tmp/public.key
-ADD ./certs/private.key /tmp/private.key
 
 ENTRYPOINT /usr/bin/cloudframe-security-vault
--- main.go
+++ main.go
@@ -3,24 +3,15 @@ package main
 import (
 	"descinet.bbva.es/cloudframe-security-vault/EventConnector"
 	"descinet.bbva.es/cloudframe-security-vault/SecretApi"
-	"path/filepath"
-	//"descinet.bbva.es/cloudframe-security-vault/utils/filesystem"
 )
 
 var (
-	DefaultPublicKey  = filepath.Join("/tmp", "public.key")
-	DefaultPrivateKey = filepath.Join("/tmp", "private.key")
-	DefaultCaCert     = filepath.Join("/tmp", "ca.cert")
 	DefaultSecretPaths = "/tmp"
 )
 
 
 func main() {
 
-	//fileUtils := filesystem.DefaultFileUtils{}
-
-
-	//secretApiHandler, err := SecretApi.NewExampleSecretApi(DefaultCaCert, DefaultPrivateKey, DefaultPublicKey, &fileUtils)
 	secretApiHandler, err := SecretApi.NewVaultSecretApi("86666040-1b49-35e8-5bb7-e4c323f48df3","http://vault-server:8200")
 	if err != nil {
 		panic(err.Error())

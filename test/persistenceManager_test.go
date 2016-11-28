f0ee01df40d5ae14ed20e0252f40953caf9b655d
Roberto Muñoz <roberto.munoz.fernandez.contractor@bbva.es>
2016-11-28 18:08:18 +0100

    FEAT: se implementa el mock de Dirutils (se recupera el creado para la release 1.*) y se crea el test de la funcion recoverlease del persistencemanager

--- test/persistenceManager_test.go
+++ test/persistenceManager_test.go
@@ -1,18 +1,69 @@
 package test
 
-//import "testing"
+import (
+	"descinet.bbva.es/cloudframe-security-vault/utils/config"
+	"github.com/facebookgo/inject"
+	"testing"
+	"descinet.bbva.es/cloudframe-security-vault/persistence"
+	"reflect"
+	"os"
+)
 
-//func TestpersistenceManager_RecoverLeases(t *testing.T) {
-//
-//	fixtures := []struct {
-//		fileUtils        FakeFileUtils
-//		expectedResponse error
-//	}{
-//		{
-//			fileUtils: FakeFileUtils{
-//				readEnv: DefaultReadEnvMetrics(),
-//			},
-//			expectedResponse: nil,
-//		},
-//	}
-//}
+func TestPersistenceManager_RecoverLeases(t *testing.T) {
+
+
+	fixtures := []struct {
+		fileUtils        FakeFileUtils
+		expectedError error
+	}{
+		{
+			fileUtils: FakeFileUtils{
+				readEnv: DefaultReadEnvMetrics(),
+				readDir: ReadDirTestMetrics{
+					content: []os.FileInfo{
+						os.FileInfo(FakeFileInfo{"test"}),
+					},
+					MethodCallMetrics: MethodCallMetrics{
+						expectedCalls: 1,
+						method: "readdir",
+					},
+				},
+
+				readFile: ReadFileTestMetrics{
+					content: "{}",
+					MethodCallMetrics: MethodCallMetrics{
+						expectedCalls: 1,
+						method: "readfile",
+					},
+				},
+
+			},
+			expectedError: nil,
+		},
+	}
+
+	for i, fixture := range fixtures {
+		cfg := &config.Config{}
+
+		if err := inject.Populate(cfg, &fixture.fileUtils); err != nil {
+			t.Error(err.Error())
+		}
+
+		cfg.ReadConfig()
+
+		persistenceCfg := &persistence.PersistenceManager{}
+		if err := inject.Populate(persistenceCfg,&fixture.fileUtils); err != nil {
+			t.Error(err.Error())
+		}
+		_, persistenceManager := persistence.NewPersistenceManager(cfg,persistenceCfg)
+
+		err := persistenceManager.RecoverLeases()
+		fixture.fileUtils.readEnv.Report(t, i)
+		fixture.fileUtils.readFile.Report(t, i)
+		fixture.fileUtils.readDir.Report(t, i)
+
+		if !reflect.DeepEqual(err, fixture.expectedError) {
+			t.Errorf("%d - Expected %v, Received %v\n", i, fixture.expectedError, err)
+		}
+	}
+}
